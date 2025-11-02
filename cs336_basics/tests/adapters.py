d77d48f1d8c606dc93ad25d17817fb90039195b3
Noé Fontana <noe.fontana.pro@gmail.com>
2025-11-02 10:14:50 -0500

    refactor: rename merge

--- cs336_basics/tests/adapters.py
+++ cs336_basics/tests/adapters.py
@@ -624,7 +624,7 @@ def run_train_bpe(
         f"Remaining {len(pretokens)} after removing special tokens"
     )
 
-    def merge(pretokens: dict[tuple[bytes, ...], int]) -> tuple[bytes, bytes] | None:
+    def single_merge(pretokens: dict[tuple[bytes, ...], int]) -> tuple[bytes, bytes] | None:
         occurences: dict[tuple[bytes, bytes], int] = {}
         for pretoken, count in pretokens.items():
             for pair in zip(pretoken, pretoken[1:]):
@@ -643,33 +643,39 @@ def run_train_bpe(
                     best_pair = max(best_pair, pair)
         return best_pair
 
-    merges: list[tuple[bytes, bytes]] = []
-    while len(vocab) < vocab_size:
-        if len(pretokens) == 0:
-            break
-        best_pair = merge(pretokens)
-        if best_pair:
-            merges.append(best_pair)
-            best_flat = b"".join(best_pair)
-            vocab[len(vocab)] = best_flat
+    def merge(
+        pretokens: dict[tuple[bytes, ...], int], initial_vocab: dict[int, bytes], max_vocab_size: int
+    ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+        merges: list[tuple[bytes, bytes]] = []
+        while len(vocab) < max_vocab_size:
+            if len(pretokens) == 0:
+                break
+            best_pair = single_merge(pretokens)
+            if best_pair:
+                merges.append(best_pair)
+                best_flat = b"".join(best_pair)
+                vocab[len(vocab)] = best_flat
 
-            new_pretokens: dict[tuple[bytes, ...], int] = {}
-            for pretoken, count in pretokens.items():
-                new_pretoken_list = []
-                i = 0
-                pretoken_length = len(pretoken)
-                while i < pretoken_length:
-                    if i < pretoken_length - 1 and (pretoken[i], pretoken[i + 1]) == best_pair:
-                        new_pretoken_list.append(best_flat)
-                        i += 2  # If a match, skip the pair
-                    else:
-                        new_pretoken_list.append(pretoken[i])
-                        i += 1
+                new_pretokens: dict[tuple[bytes, ...], int] = {}
+                for pretoken, count in pretokens.items():
+                    new_pretoken_list = []
+                    i = 0
+                    pretoken_length = len(pretoken)
+                    while i < pretoken_length:
+                        if i < pretoken_length - 1 and (pretoken[i], pretoken[i + 1]) == best_pair:
+                            new_pretoken_list.append(best_flat)
+                            i += 2  # If a match, skip the pair
+                        else:
+                            new_pretoken_list.append(pretoken[i])
+                            i += 1
 
-                new_pretoken_tuple = tuple(new_pretoken_list)
-                new_pretokens[new_pretoken_tuple] = new_pretokens.get(new_pretoken_tuple, 0) + count
-            pretokens = new_pretokens
-        else:
-            logger.warning("Unexpected: best_pair was None")
+                    new_pretoken_tuple = tuple(new_pretoken_list)
+                    new_pretokens[new_pretoken_tuple] = new_pretokens.get(new_pretoken_tuple, 0) + count
+                pretokens = new_pretokens
+            else:
+                logger.warning("Unexpected: best_pair was None")
+        return vocab, merges
+
+    vocab, merges = merge(pretokens, vocab, vocab_size)
 
     return vocab, merges
