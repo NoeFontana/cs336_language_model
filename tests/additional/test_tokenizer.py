e3f3cfa14fa5bebc06af6559f7b2d4645ab59aef
Noé Fontana <noe.fontana.pro@gmail.com>
2025-11-08 12:01:06 -0500

    feat(tokenizer): tokenizer scaffolding and base tests

---
+++ tests/additional/test_tokenizer.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_tokenizer_init():
+    pass
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_tokenizer_from_files():
+    pass
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_tokenizer_encode():
+    pass
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_tokenizer_encode_iterable():
+    pass
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_tokenizer_decode():
+    pass
