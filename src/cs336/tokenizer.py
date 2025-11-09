37098a5bbe6ddefa0b6a92bf6ed87f48171e62ff
Noé Fontana <noe.fontana.pro@gmail.com>
2025-11-09 09:21:38 -0500

    feat(tokenizer): can init from assignment format and single json

--- src/cs336/tokenizer.py
+++ src/cs336/tokenizer.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import base64
 import json
+from collections.abc import Iterable, Iterator
 
 
 class Tokenizer:
@@ -24,21 +26,40 @@ class Tokenizer:
     def from_files(
         cls,
         vocab_filepath: str,
-        merges_filepath: str,
+        merges_filepath: str | None,
         special_tokens: list[str] | None = None,
     ) -> Tokenizer:
-        with open(vocab_filepath, encoding="utf-8") as f:
-            vocab_data = json.load(f)
+        if vocab_filepath.endswith(".json") and merges_filepath is None:
+            with open(vocab_filepath, encoding="utf-8") as f:
+                data = json.load(f)
+            vocab_data = data["vocab"]
+            merges_data = data["merges"]
+            vocab = {int(k): base64.b64decode(v) for k, v in vocab_data.items()}
+            merges = [(base64.b64decode(p1), base64.b64decode(p2)) for p1, p2 in merges_data]
+        else:
+            if merges_filepath is None:
+                raise ValueError("merges_filepath must be provided if vocab_filepath is not a JSON file.")
+            with open(vocab_filepath, encoding="utf-8") as f:
+                vocab_data = json.load(f)
 
-        vocab = {int(k): v.encode("utf-8") for k, v in vocab_data.items()}
+            vocab = {int(k): v.encode("utf-8") for k, v in vocab_data.items()}
 
-        merges = []
-        with open(merges_filepath, encoding="utf-8") as f:
-            for line in f:
-                line = line.strip()
-                if not line:
-                    continue
-                p1, p2 = line.split()
-                merges.append((p1.encode("utf-8"), p2.encode("utf-8")))
+            merges = []
+            with open(merges_filepath, encoding="utf-8") as f:
+                for line in f:
+                    line = line.strip()
+                    if not line:
+                        continue
+                    p1, p2 = line.split()
+                    merges.append((p1.encode("utf-8"), p2.encode("utf-8")))
 
         return cls(vocab, merges, special_tokens)
+
+    def encode(self, text: str) -> list[int]:
+        raise NotImplementedError()
+
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        raise NotImplementedError()
+
+    def decode(self, ids: list[int]) -> str:
+        raise NotImplementedError()
